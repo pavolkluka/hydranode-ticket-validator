f936ba5d0172039e0674d1cbe99aab3f95b19b78
Pavol Kluka <pavol.kluka@gmail.com>
2025-02-15 11:41:41 -0300

    PWA - test 1

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'ticket-validator-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/js/scanner.js',
+  'https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js',
+  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch new version
+        return response || fetch(event.request)
+          .then((fetchResponse) => {
+            // Don't cache third-party resources
+            if (
+              !event.request.url.startsWith('http://') &&
+              !event.request.url.startsWith('https://')
+            ) {
+              return fetchResponse;
+            }
+
+            return caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+              });
+          });
+      })
+      .catch(() => {
+        // Return offline fallback for HTML requests
+        if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/offline.html');
+        }
+      })
+  );
+});

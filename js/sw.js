0deebaadd981336a5b4eed941365b59650f4832d
Pavol Kluka <pavol.kluka@gmail.com>
2025-02-15 12:06:45 -0300

    update PWA config

--- js/sw.js
+++ js/sw.js
@@ -6,15 +6,7 @@ const ASSETS = [
   '/js/main.js',
   '/js/scanner.js',
   'https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js',
-  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
+  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js'
 ];
 
 // Install event
